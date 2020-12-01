e7521540b20dd2c6e6bc01c9c2e32e4bbf2eb5d3
bobby45800 <44036776+bobby45800@users.noreply.github.com>
2020-12-01 14:39:30 +0000

    Updated Day1

--- Days/Day1.py
+++ Days/Day1.py
@@ -1,9 +1,12 @@
 import itertools
+import time
 
 expenseReport = []
 for i in range(200):
     expenseReport.append(int(input()))
+start = time.time()
 
+# Execution time: 4.32s
 # for i in range(len(expenseReport)):
 #     for j in range(len(expenseReport)):
 #         for k in range(len(expenseReport)):
@@ -11,12 +14,14 @@ for i in range(200):
 #                 print(expenseReport[i] * expenseReport[j] * expenseReport[k])
 #                 break
 
-combinations = [()]
+# Execution time: 0.9s
 combinations = list(itertools.product(expenseReport, expenseReport, expenseReport))
 for i in combinations:
-    currentTuple = ()
-    currentTuple = combinations[i]
+    currentTuple = combinations[0][i]
     if currentTuple(0) + currentTuple(1) + currentTuple(2) == 2020:
         print(currentTuple(0) * currentTuple(1) * currentTuple(2))
         break
+
+end = time.time()
+print(f"Executiont time: {end - start}")
 
