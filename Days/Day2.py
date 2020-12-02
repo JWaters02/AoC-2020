88212c03eba39297e585aab65fff3b267d338a03
bobby45800 <44036776+bobby45800@users.noreply.github.com>
2020-12-02 05:56:34 +0000

    Create Day2.py

---
+++ Days/Day2.py
@@ -0,0 +1,38 @@
+import time
+
+passwordList = []
+for i in range(1000):
+    passwordList.append(input())
+
+start = time.time()
+currentLine = []
+firstPos = 0
+secondPos = 0
+letter = ''
+password = ''
+numValidPasswords = 0
+for i in range(len(passwordList)): 
+    currentLine = passwordList[i].split(' ')  
+    rangeNumOfChars = currentLine[0].split('-')
+    firstPos = int(rangeNumOfChars[0])
+    secondPos = int(rangeNumOfChars[1])
+    letter = currentLine[1]
+    letter = letter.replace(":", "") 
+    password = currentLine[2]
+
+    print(currentLine)
+    print("firstpos",str(firstPos))
+    print("secondpos",str(secondPos))
+    print(letter)
+    print(password)
+
+    # if letter appears in either firstpos or second pos
+    if password[firstPos - 1] == letter or password[secondPos - 1] == letter:
+        # if not letter appears in both firstpos and second pos (cancels out if both are there)
+        if not (password[firstPos - 1] == letter and password[secondPos - 1] == letter):
+            numValidPasswords += 1
+            print("valid")
+
+end = time.time()
+print(numValidPasswords)
+print(f"Executiont time: {end - start}")
