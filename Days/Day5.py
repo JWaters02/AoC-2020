1e0d7e0cec0e2013466e59f9c21b3afdfc295c4a
bobby45800 <44036776+bobby45800@users.noreply.github.com>
2020-12-05 05:42:32 +0000

    Create Day5.py

---
+++ Days/Day5.py
@@ -0,0 +1,24 @@
+import time
+
+lines = []
+for i in range(897):
+    lines.append(input())
+
+start = time.time()
+seats = set()
+for line in lines:
+    line = line.replace('F', '0')
+    line = line.replace('B', '1')
+    line = line.replace('L', '0')
+    line = line.replace('R', '1')
+    num = int(line, 2)
+    seats.add(num)
+
+for i in range(256 * 8):
+    if (i not in seats):
+        if i + 1 in seats:
+            if i - 1 in seats:
+                print(i)
+
+end = time.time()
+print(f"Executiont time: {end - start}")
