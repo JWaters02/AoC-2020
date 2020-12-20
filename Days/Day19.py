32aeac2c5839cd44350abc62a626104c9fa864c1
JWaters02 <44036776+JWaters02@users.noreply.github.com>
2020-12-20 13:51:30 +0000

    Create Day19

--- Days/Day19.py
+++ Days/Day19.py
@@ -1,4 +1,5 @@
 import fileinput
+import itertools
 import time
 
 def generateCasesFromRules(ruleNumber, case, ruleStrings):
@@ -21,17 +22,43 @@ def generateCasesFromRules(ruleNumber, case, ruleStrings):
         validRules[ruleNumber, case] = False
     return validRules[ruleNumber, case]
 
-def checkAgainstCases(caseStrings, ruleStrings):
-    validCases = 0
+def generateNewCasesFromNewRules(ruleNumber, ruleStrings):
+    rulePos = {}
+    if ruleNumber in rulePos:
+        return rulePos[ruleNumber]
+    if type(ruleStrings[ruleNumber]) is str:
+        rulePos[ruleNumber] = [ruleStrings[ruleNumber]]
+        return rulePos[ruleNumber]
+    rulePos[ruleNumber] = []
+    for vd in ruleStrings[ruleNumber]:
+        rulePos[ruleNumber].extend([''.join(newLine) for newLine in itertools.product(*[generateNewCasesFromNewRules(newRule, ruleStrings) for newRule in vd])])
+    return rulePos[ruleNumber]
+
+def handleLoopingRules(ruleStrings, caseStrings):
+    rule42 = generateNewCasesFromNewRules(42, ruleStrings)
+    rule31 = generateNewCasesFromNewRules(31, ruleStrings)
+    c = 0
     for case in caseStrings:
-        if generateCasesFromRules(0, case, ruleStrings):
-            validCases += 1
-    return validCases
+        if len(case)%8 != 0:
+            continue
+        bc = [case[i:i+8] for i in range(0,len(case),8)]
+        dx = 0
+        c42 = 0
+        while dx < len(bc) and bc[dx] in rule42:
+            dx += 1
+            c42 += 1
+        c31 = 0
+        while dx < len(bc) and bc[dx] in rule31:
+            dx += 1
+            c31 += 1
+        if dx == len(bc) and c31 < c42 and c31 > 0:
+            c += 1
+    return c
 
 def main():
     start = time.time()
 
-    # Execution time: 210s
+    # Execution time: 0.02s
     with open("Day19Input.txt") as line:
         lines = line.read().strip("\n")
     rules, cases = lines.split("\n\n")
@@ -45,9 +72,8 @@ def main():
             rulesStrings[int(leftRules)] = rightRules[0].strip('"')
         else:
             rulesStrings[int(leftRules)] = [[int(rulePointer) for rulePointer in rule.split(" ")] for rule in rightRules]
-
-    print(checkAgainstCases(caseStrings, rulesStrings))
-
+    print(handleLoopingRules(rulesStrings, caseStrings))
+    
     end = time.time()
     print(f"Executiont time: {end - start}")
 
